a9bdd658bf8290ad3d066071d79e9e5958b33514
Kirill Chausov <kirill.chausov2@gmail.com>
2020-09-09 00:47:52 +0300

    add line

--- src/ru/mirea/prak1_2/Main.java
+++ src/ru/mirea/prak1_2/Main.java
@@ -2,6 +2,13 @@ package ru.mirea.prak1_2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        double result = 0;
+        for(double j = 1; j <= 10; j++){
+            System.out.print((int)j + ") ");
+            System.out.format("%.3f%n", 1/j);
+            result += 1/j;
+        }
+        System.out.print("\n" + "Сумма первых десяти чисел гармонического ряда: ");
+        System.out.format("%.3f%n", result);
     }
 }
