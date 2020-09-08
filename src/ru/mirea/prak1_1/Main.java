30b38031a410fe9e208a8c015ebacf952b76728c
Kirill Chausov <kirill.chausov2@gmail.com>
2020-09-09 00:12:23 +0300

    add line

--- src/ru/mirea/prak1_1/Main.java
+++ src/ru/mirea/prak1_1/Main.java
@@ -2,7 +2,29 @@ package ru.mirea.prak1_1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-        System.out.println("World Hello!!!");
+        int[] arr = {10, 20, 30, 40, 50};
+        int result_1 = 0, result_2 = 0, result_3 = 0;
+
+        for(int j = 0; j < arr.length; ++j){
+            result_1 += arr[j];
+        }
+
+        int j = 0;
+
+        while(j < arr.length){
+            result_2 += arr[j];
+            j++;
+        }
+
+        j = 0;
+
+        do{
+            result_3 += arr[j];
+            j++;
+        }while(j < arr.length);
+
+        System.out.println("Сумма элементов целочисленного массива, полученная с помощью цикла for: " + result_1);
+        System.out.println("Сумма элементов целочисленного массива, полученная с помощью цикла while: " + result_2);
+        System.out.println("Сумма элементов целочисленного массива, полученная с помощью цикла do while: " + result_3);
     }
 }
--- src/ru/mirea/prak1_4/Main.java
+++ src/ru/mirea/prak1_4/Main.java
@@ -1,6 +1,16 @@
 package ru.mirea.prak1_4;
 
 public class Main {
+    public static long fact(int n){
+        int result = 1;
+        if(n >= 1){
+            for(int j = 2; j <= n; j++){
+                result *= j;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
     }
