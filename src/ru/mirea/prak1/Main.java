8dbe4e3fcc146b481a68dbed767b524ae95103d5
Kirill Chausov <kirill.chausov2@gmail.com>
2020-09-08 22:55:28 +0300

    add line

--- src/ru/mirea/prak1/Main.java
+++ src/ru/mirea/prak1/Main.java
@@ -3,5 +3,6 @@ package ru.mirea.prak1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("World Hello!!!");
     }
 }
